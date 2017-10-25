d00768e55928f0991d86acad45aef14e4538b113
Roman Tsvyk <roma.tsvyk@gmail.com>
2017-10-25 09:59:43 +0300

    fixed redux-router

--- src/router/RootRouter.tsx
+++ src/router/RootRouter.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {Provider} from "react-redux";
 import {ConnectedRouter} from "react-router-redux";
-import {Switch} from "react-router-dom";
 import storeConfig from "../store/storeConfig";
 import createBrowserHistory from 'history/createBrowserHistory';
 
@@ -13,9 +12,7 @@ export const history = createBrowserHistory();
 const RootRouter = () => (
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <Switch>
-                <Layout/>
-            </Switch>
+            <Layout/>
         </ConnectedRouter>
     </Provider>
 );
